60620584fac115b6abc5a8b16a237be1e4a6dbf6
unrealwill <william.ribault@gmail.com>
2022-12-21 21:04:40 +0100

    Update x.sh
    Merry winter solstice !

--- x.sh
+++ x.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-head -n2 $0 | diff - /dev/null
-exit 0
+echo "#!/bin/sh
+head -n2 \$0 | diff - /dev/null
+exit 0" > xxx.sh
+ping orchid.gistnoesis.net -c 1 > ping.txt 2>&1
+diff $0 xxx.sh  
